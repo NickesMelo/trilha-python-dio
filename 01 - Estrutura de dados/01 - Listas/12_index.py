601797c5d6beb0c599e3885c70bb2162284a6a4e
Nicolas Melo <141958895+NickesMelo@users.noreply.github.com>
2024-10-01 10:25:36 -0300

    def funcao

--- 01 - Estrutura de dados/01 - Listas/12_index.py
+++ 01 - Estrutura de dados/01 - Listas/12_index.py
@@ -2,3 +2,7 @@ linguagens = ["python", "js", "c", "java", "csharp"]
 
 print(linguagens.index("java"))  # 3
 print(linguagens.index("python"))  # 0
+
+
+def funcao(*args, **kw):
+    funcao("python", 2022, curso="dio")
